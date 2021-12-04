92c9755b7bd7d6966933f0fab16238e9edd51120
mograine <mogriaine@ididinc.com>
2021-12-04 18:51:01 +0900

    Initial Commit

---
+++ SwfitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwfitProject
+//
+//  Created by mograine on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
