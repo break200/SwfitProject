b8cbdfcea466ee45d91d7b73c46f63cf031d437c
mograine <mogriaine@ididinc.com>
2021-12-13 00:10:27 +0900

    @IBDesignable extension UIView 추가

---
+++ SwfitProject/Util/UIViewEx.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  SwfitProject
+//
+//  Created by mograine on 2021/12/13.
+//
+
+import UIKit
+
+@IBDesignable
+extension UIView{
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
